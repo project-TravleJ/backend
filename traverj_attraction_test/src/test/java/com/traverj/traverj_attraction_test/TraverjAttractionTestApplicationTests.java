76b4118521ae3d15dcafb1d1c825ad45c920cd55
leebumsung <leebumsung3@naver.com>
2023-04-11 11:12:32 +0900

    초기파일들 stage로 commit

---
+++ traverj_attraction_test/src/test/java/com/traverj/traverj_attraction_test/TraverjAttractionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traverj.traverj_attraction_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraverjAttractionTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
