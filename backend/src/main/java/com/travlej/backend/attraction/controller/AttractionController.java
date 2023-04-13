b7e1384fdd71cd431ecd91ba1d848ff3b84d1a15
JaeHyeon Ryu <elqm9016@gmail.com>
2023-04-13 19:38:45 +0900

    [fix] 백엔드 파일 에러 수정
    수정 완료.

--- backend/src/main/java/com/travlej/backend/attraction/controller/AttractionController.java
+++ backend/src/main/java/com/travlej/backend/attraction/controller/AttractionController.java
@@ -4,6 +4,7 @@ import com.travlej.backend.attraction.dto.AttractionDTO;
 import com.travlej.backend.attraction.service.AttractionService;
 import com.travlej.backend.common.ResponseDto;
 import com.travlej.backend.post.dto.PostDTO;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- backend/src/main/java/com/travlej/backend/member/dto/RequestDTO.java
+++ backend/src/main/java/com/travlej/backend/member/dto/RequestDTO.java
@@ -1,14 +1,14 @@
-//package com.travlej.backend.member.dto;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//public class RequestDTO {
-//    private String memberId;
-//
-//    private String memberNickname;
-//
-//    private String memberPwd;
-//}
+package com.travlej.backend.member.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RequestDTO {
+    private String memberId;
+
+    private String memberNickname;
+
+    private String memberPwd;
+}
