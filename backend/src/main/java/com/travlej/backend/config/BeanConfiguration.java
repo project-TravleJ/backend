34b7dc221948100e4b5f1709c18b4e9ffd6dd9f5
JaeHyeon Ryu <elqm9016@gmail.com>
2023-04-13 09:53:28 +0900

    [feat] 회원가입 기능 구현
    구현 중...

---
+++ backend/src/main/java/com/travlej/backend/config/BeanConfiguration.java
@@ -0,0 +1,17 @@
+package com.travlej.backend.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.travlej.backend")
+public class BeanConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper() {
+
+        return new ModelMapper();
+    }
+}
