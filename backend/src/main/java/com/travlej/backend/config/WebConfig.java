3fd82b162f300f7684fe15aa849c40aa6dbde786
jisoo <skscjswo32@gmail.com>
2023-04-24 11:04:22 +0900

    backend bugfix 완료

--- backend/src/main/java/com/travlej/backend/config/WebConfig.java
+++ backend/src/main/java/com/travlej/backend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001")
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
 //                .allowCredentials(false)
