2a633406f456f5d892222fccd04abdfbd727229a
chchcoco <117333258+chchcoco@users.noreply.github.com>
2023-04-26 09:32:28 +0900

    [fix] course 업데이트 API 추가 및 수정

--- backend/src/main/java/com/travlej/backend/course/controller/CourseController.java
+++ backend/src/main/java/com/travlej/backend/course/controller/CourseController.java
@@ -32,22 +32,28 @@ public class CourseController {
         return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "코스 전체 조회 완료", courseService.findCourses()));
     }
 
-//    @GetMapping("/posts/{postId}/courses")
-//    public ResponseEntity<ResponseDto> selectCourseByPostId(@PathVariable int postId){
-//
-//        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "게시글 코스 조회 완료", courseService.findCourseByPostId(postId)));
-//    }
-//
-//    @PostMapping("/posts/{postId}/courses")
-//    public ResponseEntity<ResponseDto> registCourse(@PathVariable int postId, @RequestBody List<CourseDTO> courseDTOList){
-//
-//        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "코스 등록 성공", courseService.registNewCourseList(postId, courseDTOList)));
-//    }
+    @GetMapping("/posts/{postId}/courses")
+    public ResponseEntity<ResponseDto> selectCourseByPostId(@PathVariable int postId){
+
+        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "게시글 코스 조회 완료", courseService.findCourseByPostId(postId)));
+    }
+
+    @PostMapping("/posts/{postId}/courses")
+    public ResponseEntity<ResponseDto> registCourse(@PathVariable int postId, @RequestBody List<CourseDTO> courseDTOList){
+
+        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "코스 등록 성공", courseService.registNewCourseList(postId, courseDTOList)));
+    }
 
     @DeleteMapping("/posts/{postId}/courses")
     public ResponseEntity<ResponseDto> deleteCourseByPostId(@PathVariable int postId){
 
         return ResponseEntity.ok().body(new ResponseDto(HttpStatus.NO_CONTENT, "코스 삭제 완료", courseService.deleteCourses(postId)));
+    }
+
+    @PutMapping("/posts/{postId}/courses")
+    public ResponseEntity<ResponseDto> updateCourseByPostId(@PathVariable int postId, @RequestBody List<CourseDTO> courseDTOList){
+
+        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.CREATED, "코스 수정", courseService.updateCourseList(postId, courseDTOList)));
     }
 
 }
